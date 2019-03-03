cd25bd5963c67074596ec694c6b2ae1666e8cd98
Alejandro <alesitonightray@gmail.com>
2019-03-03 13:42:35 +0100

    StudentDao implement BaseDao

---
+++ app/src/main/java/com/iessaladillo/alejandro/adm_pr10_fct/data/local/StudentDao.java
@@ -0,0 +1,7 @@
+package com.iessaladillo.alejandro.adm_pr10_fct.data.local;
+
+import com.iessaladillo.alejandro.adm_pr10_fct.base.BaseDao;
+import com.iessaladillo.alejandro.adm_pr10_fct.data.local.model.Student;
+
+public interface StudentDao extends BaseDao<Student> {
+}
