74111ec8e6fafd00ffcf012b5d46ba8dbd27ff04
Alejandro <alesitonightray@gmail.com>
2019-03-05 20:17:16 +0100

    destino inicial dinamico

--- app/src/main/java/com/iessaladillo/alejandro/adm_pr10_fct/ui/main/MainActivity.java
+++ app/src/main/java/com/iessaladillo/alejandro/adm_pr10_fct/ui/main/MainActivity.java
@@ -7,9 +7,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
+import androidx.navigation.NavInflater;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -26,13 +30,61 @@ public class MainActivity extends AppCompatActivity implements ToolbarConfigurat
     private DrawerLayout drawerLayout;
     private static final String PREFERENCES_FILE = "prefs";
     private static final String PREF_NAV_DRAWER_OPENED = "navdrawerOpened";
+    private SharedPreferences.OnSharedPreferenceChangeListener onSharePreferencesChangeListener;
+    private SharedPreferences settings;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        settings.registerOnSharedPreferenceChangeListener(onSharePreferencesChangeListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        settings.registerOnSharedPreferenceChangeListener(onSharePreferencesChangeListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        setupNavigationGraph();
         navController = Navigation.findNavController(this, R.id.navHostFragment);
         setupViews();
+    }
+
+    private void setupNavigationGraph() {
+        int startDestinationResId = 0;
+
+        NavHostFragment navHost =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(
+                        R.id.navHostFragment);
+
+        NavController navController = navHost.getNavController();
+
+        NavInflater navInflater = navController.getNavInflater();
+
+        NavGraph navGraph = navInflater.inflate(R.navigation.main_navigation);
+
+        switch (settings.getString(getString(R.string.prefStartingScreen_key), getString(R.string.prefStartingScreen_defaultValue))) {
+            case "nextVisits":
+                startDestinationResId = R.id.nextVisitsFragment;
+                break;
+            case "students":
+                startDestinationResId = R.id.studentsFragment;
+                break;
+            case "companies":
+                startDestinationResId = R.id.companiesFragment;
+                break;
+            case "visits":
+                startDestinationResId = R.id.visitsFragment;
+                break;
+        }
+
+        navGraph.setStartDestination(startDestinationResId);
+        navController.setGraph(navGraph);
     }
 
     private void setupViews() {
